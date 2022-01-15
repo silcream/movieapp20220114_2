126f654c2e4444b5c75009c0ef03557fbc5de270
silcream <81163573+silcream@users.noreply.github.com>
2022-01-15 20:07:05 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import react from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+  
+  state = {
+    count : 0,
+  };
+
+  add = () => {
+    this.setState(current => ({ count : this.state.count + 1}));
+  }
+
+  minus = () => {
+    this.setState(current => ({ count : this.state.count -1}));
+  }
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  componentWillUnmount(){
+    console.log('Goodbye');
+  }
+  
+  render() {
+    console.log("I am rendering");
+    return (
+      <div>
+        <h1> The number is : {this.state.count} </h1>
+        <button onClick = {this.add}>Add</button>
+        <button onClick = {this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+export default App;
